11904643511b3463f3dda35a9aea9e104d4acd96
masafumimori <m.masafumimori@outlook.com>
2023-04-08 21:19:47 +0900

    add main

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from utils.logger import Logger
+
+import time
+from datetime import datetime
+
+def main():
+    try:
+        Logger().info(f'Start running at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
+        
+        # Code for running bot
+
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    main()
