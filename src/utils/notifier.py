a6c2b9d32a8c99934e8d5e40287d631a85934939
masafumimori <m.masafumimori@outlook.com>
2023-04-12 07:57:33 +0900

    add LINE notifier

--- src/utils/notifier.py
+++ src/utils/notifier.py
@@ -21,3 +21,46 @@ class Discord(Notifier):
         headers = {'Content-Type': 'application/json'}
 
         requests.post(self.__webhookurl, json.dumps(content), headers=headers)
+
+
+class Line(Notifier):
+    """
+    Notifier object that sends messages to a LINE user or group.
+    """
+
+    BASE_URL = 'https://notify-api.line.me/api/notify'
+
+    def __init__(self, token: str) -> None:
+        """
+        Initializes the Line notifier instance with an access token.
+
+        Parameters:
+        - `token`: The Line access token to use.
+        """
+        self.token = token
+
+    def send(self, msg: str) -> None:
+        """
+        Sends a message to the Line user or group.
+
+        Parameters:
+        - `msg`: The message to send.
+        """
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Authorization": "Bearer " + self.token
+        }
+        requests.post(self.BASE_URL, headers=headers, data={"message": self._safe_msg(msg)})
+
+    def _safe_msg(self, msg: str) -> str:
+        """
+        Returns a truncated message if it exceeds the maximum allowable length for a Line message.
+
+        Parameters:
+        - `msg`: The message to truncate, if necessary.
+
+        Returns:
+        - The original message, truncated to fit within the Line message character limit.
+        """
+        # Only allows 1000 chars
+        return msg[:1000]
