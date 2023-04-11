580f892fed67bf02d95ff5835a4c820dcaf82ebd
masafumimori <m.masafumimori@outlook.com>
2023-04-12 08:28:43 +0900

    change singleton pattern

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,11 +1,11 @@
 import os
 from datetime import datetime
-from typing import Callable, Dict, Optional, List
+from typing import Callable, Dict, List, Optional
 from loguru import logger
 from notifiers.logging import NotificationHandler
 from notifier import Discord, LINE, Notifier
 
-gmail_notofier_params = {
+GMAIL_NOTIFIER_PARAMS = {
     "username": os.getenv('GMAIL_USERNAME'),
     "password":  os.getenv('GMAIL_PASSWORD'),
     "to":  os.getenv('NOFITY_TO')
@@ -21,31 +21,63 @@ FILEPATHS = {
 class Logger:
 
     _instance = None
-    notifiers: List[Notifier] = []
+    _notifiers: List[Notifier] = []
 
-    def __new__(cls, notifiers=["discord"]):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
+    @classmethod
+    def instance(cls, notifiers: Optional[List[str]] = None) -> 'Logger':
+        """
+        Returns the singleton instance of the `Logger` class.
 
-            logger.remove()
-            logger.add(FILEPATHS["info"], rotation="500 MB")
+        If the `notifiers` config list is specified and is different from the previously set configuration,
+        the `Logger` instance is recreated with the new list.
 
-            # Custom sink function to create a new error log file with timestamp
-            def error_sink(message):
-                timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-                with open(f"log/error_{timestamp}.log", "w") as error_file:
-                    error_file.write(message)
+        Parameters:
+            `notifiers` (optional): list of notifiers to set. Possible values are `"gmail"`, `"discord"`, `"line"`.
 
-            logger.add(error_sink, level="ERROR")
-            cls._instance.notifiers = cls._instance.__set_notifiers(notifiers)
+        Returns:
+            The singleton instance of the `Logger` class.
+        """
+        if notifiers is None:
+            notifiers = ["discord"]
 
+        if not cls._instance:
+            cls._instance = cls(notifiers)
+        else:
+            instance_notifiers_classes = [n.__class__.__name__.lower() for n in cls._instance.notifiers]
+            if set(instance_notifiers_classes) != set(notifiers):
+                cls._instance._notifiers = cls._instance.__set_notifiers(notifiers)
         return cls._instance
+    
+    def __init__(self, notifiers: Optional[List[str]] = None) -> None:
+        """
+        Initializes the Logger instance with the given notifiers configuration.
+
+        Parameters:
+            `notifiers` (optional): list of notifiers to set. Possible values are `"gmail"`, `"discord"`, `"line"`.
+        """
+        if notifiers is None:
+            notifiers = ["discord"]
+
+        self.__setup_logger()
+        self._notifiers = self.__set_notifiers(notifiers)
+
+    def __setup_logger(self):
+        logger.remove()
+        logger.add(FILEPATHS["info"], rotation="500 MB")
+
+        # Custom sink function to create a new error log file with timestamp
+        def error_sink(message):
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            with open(f"log/error_{timestamp}.log", "w") as error_file:
+                error_file.write(message)
+
+        logger.add(error_sink, level="ERROR")
 
     def __set_notifiers(self, notifiers) -> List[Notifier]:
         notifier_setup_funcs: Dict[str, Callable[[], Optional[Notifier]]] = {
             "gmail": self.__set_gmail_notifier,
-            "discord": self._get_discord_notifier,
-            "line": self._get_line_notifier
+            "discord": self.__get_discord_notifier,
+            "line": self.__get_line_notifier
             # Add other notifiers and their setup functions here
         }
 
@@ -62,13 +94,13 @@ class Logger:
         return notifiers_set
 
     def __set_gmail_notifier(self) -> None:
-        handler = NotificationHandler("gmail", defaults=gmail_notofier_params)
+        handler = NotificationHandler("gmail", defaults=GMAIL_NOTIFIER_PARAMS)
         logger.add(handler, level="ERROR")
-
-    def _get_discord_notifier(self) -> Notifier:
+    
+    def __get_discord_notifier(self) -> Notifier:
         return Discord(DISCORD_WEBHOOK_URL)
     
-    def _get_line_notifier(self) -> Notifier:
+    def __get_line_notifier(self) -> Notifier:
         return LINE(LINE_NOTIFY_TOKEN)
 
     def info(self, msg):
